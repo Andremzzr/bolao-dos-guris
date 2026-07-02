ecc87e90a114d0b37cee7ab86cb7ccd857abd7c7
Andremzzr <andremezzaliraribeiro@gmail.com>
2026-07-02 16:35:23 -0300

    fix estadios weather

--- src/data/stadiumCoords.js
+++ src/data/stadiumCoords.js
@@ -2,8 +2,11 @@ export const stadiumCoords = {
   'Estádio Cidade do México, Cidade do México': { lat: 19.3029, lon: -99.1505 }, // Azteca
   'Estádio Guadalajara, Guadalajara': { lat: 20.6816, lon: -103.4627 }, // Akron
   'Estádio Toronto, Toronto': { lat: 43.6332, lon: -79.4186 }, // BMO Field
+  'Estádio de Toronto , Toronto': { lat: 43.6332, lon: -79.4186 }, // BMO Field (alias)
   'Estádio Los Angeles, Los Angeles': { lat: 33.9535, lon: -118.3390 }, // SoFi
+  'Estádio de Los Angeles, Los Angeles': { lat: 33.9535, lon: -118.3390 }, // SoFi (alias)
   'Estádio Baía de São Francisco, Santa Clara': { lat: 37.4030, lon: -121.9698 }, // Levi's
+  'Estádio da Baía de São Francisco, Área da baía de São Francisco': { lat: 37.4030, lon: -121.9698 }, // Levi's (alias)
   'Estádio Nova York Nova Jersey, East Rutherford': { lat: 40.8128, lon: -74.0745 }, // MetLife
   'Estádio Boston, Foxborough': { lat: 42.0909, lon: -71.2643 }, // Gillette
   'Estádio Vancouver, Vancouver': { lat: 49.2768, lon: -123.1120 }, // BC Place
@@ -12,7 +15,9 @@ export const stadiumCoords = {
   'Estádio Filadélfia, Filadélfia': { lat: 39.9008, lon: -75.1675 }, // Lincoln Financial
   'Estádio Monterrey, Monterrey': { lat: 25.6644, lon: -100.2444 }, // BBVA
   'Estádio Atlanta, Atlanta': { lat: 33.7554, lon: -84.4006 }, // Mercedes-Benz
+  'Estádio de Atlanta, Atlanta': { lat: 33.7554, lon: -84.4006 }, // Mercedes-Benz (alias)
   'Estádio Miami, Miami': { lat: 25.9580, lon: -80.2389 }, // Hard Rock
   'Estádio Seattle, Seattle': { lat: 47.5952, lon: -122.3316 }, // Lumen Field
+  'Estádio de Seattle, Seattle': { lat: 47.5952, lon: -122.3316 }, // Lumen Field (alias)
   'Estádio Kansas City, Kansas City': { lat: 39.0489, lon: -94.4839 } // Arrowhead
 };
